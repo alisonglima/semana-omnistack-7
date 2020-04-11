b7a1e58bde47c72cda60fed3ac0a54861491a765
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-04-11 12:06:11 -0300

    Primeiro commit do app mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Plataform, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createAppContaeiner,
+  createStackNavigator,
+} from '@react-navigation/native';
+
+import Routes from './routes';
